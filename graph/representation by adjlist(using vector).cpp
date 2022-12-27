fcff21fda5885aa03e2f763038d30bf94d30029f
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2022-12-27 20:45:50 +0530

    Create representation by adjlist(using vector).cpp

---
+++ graph/representation by adjlist(using vector).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,e;
+	cin>>n>>e;
+	
+	vector<int>adj[n];
+	
+	for(int i =0;i<e;i++)
+	{
+		int x,y;
+		cin>>x>>y;
+		
+		adj[x].push_back(y);
+	}
+	
+	//printing
+	for(int i = 0; i<n;i++)
+	{
+		cout<<i<< "->";
+		for(int j = 0; j<adj[i].size();j++)
+		cout<<adj[i][j]<<" ";
+	}
+	cout << endl;
+	return 0;
+}
