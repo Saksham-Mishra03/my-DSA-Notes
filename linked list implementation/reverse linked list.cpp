4dc026cc21466a42ddc6681fe6af21e67e8acd19
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2023-05-31 11:49:26 +0530

    Update reverse linked list.cpp

--- linked list implementation/reverse linked list.cpp
+++ linked list implementation/reverse linked list.cpp
@@ -27,3 +27,21 @@ public:
 
     }
 };
+
+//recursive method
+
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+
+	if(head==NULL||head->next==NULL)
+	return head;
+
+	Node* smallans = reverseLinkedListRec(head->next);
+	
+	Node* tail = head->next;
+	tail->next=head;  //head->next->next=head;
+	head->next=NULL;
+	return smallans;
+
+}
